d8205855ca2b85f5cadcb9f76aa10f5abfb68702
DibbenduBanikPurna <77313185+DibbenduBanikPurna@users.noreply.github.com>
2021-02-09 09:57:12 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import React from 'react'
+import Pubg from './image/img.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img  src={Pubg} className="App-logo" alt="logo" />
+    
+        <div>
+        <button onClick={()=>alert('and get chicken')}>Play Pubg</button>
+        </div>
+      
+       
+       
       </header>
     </div>
   );
